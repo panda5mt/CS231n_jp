4fb333ad6a1655cb8f9313e275dee003095eaf23
Atsushi Saitoh <>
2021-03-18 16:11:02 +0900

    added some sample code

--- sample/8_PuttingItTogether/toy_2d_spiral_NN.m
+++ sample/8_PuttingItTogether/toy_2d_spiral_NN.m
@@ -1,7 +1,8 @@
-%% 線形分類テスト
+%% ニューラルネットワークテスト
 close all; 
 clc;
 
+%% パラメータ初期化
 N = 100;    % 各分類ごとのポイント数
 D = 2;      % 次元
 K = 3;      % 分類数
@@ -11,13 +12,17 @@ y = zeros(N*K, 1);  % 分類ラベル(教師ラベル)
 
 num_examples = size(X, 1);
 
-%% 線形分類器の訓練
-W = 0.01 * randn(D, K);
-b = zeros(1, K);
+%% パラメータの初期化
+h = 100; % 隠れ層サイズ
+W = 0.01 * randn(D, h);
+b = zeros(1, h);
+W2 = 0.01 * randn(h, K);
+b2 = zeros(1, K);
 
 % ハイパーパラメータの設定
 step_size = 1;
 reg = 1e-3; % 正則化強度
+
 
 %% スパイラルの行列を作成
 for j=0:(K-1)
@@ -40,10 +45,17 @@ figure(1);
 scatter(X(:,1),X(:,2),[],y_color,'filled','MarkerEdgeColor',[0 0 0],'LineWidth',1.5);
 
 %% 勾配降下計算ループ
-for i = 1:200
-    scores = X * W + b; %　分類スコアの評価
+for i = 1:10000
+    
+    %% 分類スコアの評価
+    M = X * W + b;
+    hidden_layer = M .* (0 < M); % ReLU:活性化関数
+    scores = hidden_layer * W2 + b2;
+    
+    % 分類性能計算
     exp_scores = exp(scores);
     probs = exp_scores ./ sum(exp_scores, 2); %[N x K]
+    
     
     % 損失関数計算
     correct_logprobs = zeros(num_examples,1);
@@ -54,11 +66,11 @@ for i = 1:200
     
     data_loss = sum(correct_logprobs,'all') / num_examples;
     
-    reg_loss = 0.5 * reg * sum (W .^ 2,'all');
+    reg_loss = 0.5 * reg * sum (W .^ 2,'all') + 0.5 * reg * sum (W2 .^ 2,'all');
     loss = data_loss + reg_loss;
     
     %% 10回ごとにどのくらい損失関数が改善したか表示する
-    if 0 == mod(i, 10) || (1 == i)
+    if 0 == mod(i, 1000) || (1 == i)
         Str1 = ['iteration ',num2str(i),': loss ',num2str(loss),'.'];
         disp(Str1); 
     end
@@ -71,17 +83,31 @@ for i = 1:200
     
     dscores = dscores ./ num_examples;
     
+    dW2 = hidden_layer' * dscores;
+    db2 = sum(dscores, 1);
     
-    dW = X' * dscores;
-    db =  sum (dscores, 1);
+    dhidden = dscores * W2';
+    dhidden = dhidden .* (hidden_layer > 0);
     
+    dW = X' * dhidden;
+    db = sum(dhidden, 1);
+    
+    dW2 = dW2 + reg .* W2;
     dW = dW + reg .* W;
     
     W = W - step_size * dW;
     b = b - step_size * db;
+    W2 = W2 - step_size * dW2;
+    b2 = b2 - step_size * db2;
+    
+    
 end
 %% 訓練終わり
-scores = X * W + b;
+M = X * W + b;
+hidden_layer = M .* (0 < M); % ReLU:活性化関数
+
+scores = hidden_layer * W2 + b2;
+
 [~,predicted_class] = max(scores,[],2);
 
 % 正解している数
@@ -101,16 +127,15 @@ for lp1=1:10
     DX(:,2)= sample_y;
 
 
-    s_scores = DX * W + b; %　分類スコアの評価
-    exp_scores = exp(s_scores);
-    probs = exp_scores ./ sum(exp_scores, 2); %[N x K]
+    M = DX * W + b;
+    hidden_layer = M .* (0 < M); % ReLU:活性化関数
+    scores = hidden_layer * W2 + b2;
+    [~,argmax_scores] = max(scores,[],2);
 
-    [~,argmax_probs] = max(probs,[],2);
-
+   
     % one-hot表記へ変換
-    y_color = argmax_probs==1:3;
-    
-    hold on % 先程のグラフに上書きする
+    y_color = argmax_scores==1:3;
+    hold on
     figure(1);scatter(DX(:,1),DX(:,2),[],y_color);
     hold off
 end
